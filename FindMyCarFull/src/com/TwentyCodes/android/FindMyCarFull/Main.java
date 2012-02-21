6d86d5143af2865da6b46e2ef4ec366226f6a490
Ricky Barrette <rickbarrette@gmail.com>
2012-02-21 10:34:07 -0500

    Cleaned up packages and fixed compiling errors
    Change-Id: I4fa5aa02ccbe097947ec2d846cd7b3ff0a16bde5
    Signed-off-by: Ricky Barrette <rickbarrette@gmail.com>

---
+++ FindMyCarFull/src/com/TwentyCodes/android/FindMyCarFull/Main.java
@@ -0,0 +1,14 @@
+/**
+ * Main.java
+ * @date Feb 21, 2012
+ * @author ricky barrette
+ * @author Twenty Codes, LLC
+ */
+package com.TwentyCodes.android.FindMyCarFull;
+
+/**
+ * @author ricky barrette
+ */
+public class Main extends com.TwentyCodes.android.FindMyCarLib.Main {
+
+}
